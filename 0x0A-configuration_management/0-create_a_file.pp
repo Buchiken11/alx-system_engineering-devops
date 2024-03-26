f0c929fd5f57f39feec5210649e8abf4dcf85273
Buchiken11 <buchiken025@gmail.com>
2024-03-27 00:16:45 +0100

    tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Create a file
+# Create a file in /tmp
 
-file { "school":,
-  ensure => "present",
-  mode => "0744",
-  owner => "www-data"
-  group => "www-data"
-  content => "I love Puppet"
-  path => "/tmp/school"
-}
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}}
