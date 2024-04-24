ceed142d52fab9344d3bc123fa22602558afd38c
buhiken11 <buhiken025@gmail.com>
2024-04-24 23:49:27 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,10 +13,16 @@ if __name__ == '__main__':
 
     url = "https://jsonplaceholder.typicode.com/"
     employees_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employees_id)).json()
-    parameters = {"userId": employees_id}
-    todos = requests.get(url + "todos", parameters).json()
-    done_tasks = [t.get("title") for t in todos if t.get("done_tasks") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(done_tasks), len(todos)))
-    [print("\t {}".format(done)) for done in done_tasks]
+    user = requests.get(url + "users/{}".format(employees_id))
+    user_response = user.json()
+    params = {"userId": employees_id}
+    todos = requests.get(url + "to_do_response", params=params)
+    to_do_response = todos.json()
+    done_tasks = []
+    for todo in to_do_response:
+        if todo.get("done_tasks") is True:
+            done_tasks.append(todo.get("title"))
+    print("Employee {} is done with tasks({}/{})".format(
+        user_response.get("name"), len(done_tasks), len(to_do_response)))
+    for done in done_tasks:
+        print("\t {}".format(done))
