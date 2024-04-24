448e211e8051f3daf2b8138dc52795de0a7ce46f
buhiken11 <buhiken025@gmail.com>
2024-04-25 00:42:10 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    params = {"userId": user_id}
+    todos = requests.get(url + "todos", params).json()
+    data_to_export = {
+            user_id: [
+                {
+                    "task": t.get("title"),
+                    "completed": t.get("completed"),
+                    "username": username
+                }
+                for t in todos
+            ]
+        }
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(data_to_export, jsonfile, indent=4)
+
