b5492889d202a50f1dfcbcf04e794cba6bb32b2c
Buchiken11 <buchiken025@gmail.com>
2024-05-08 04:06:44 +0100

    first commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,7 +3,7 @@
 A function that queries the Reddit API and prints the titles of the
 first 10 hot posts listed for a given subreddit
 """
-import reqeust
+import reqeusts
 
 
 def top_ten(subreddit):
