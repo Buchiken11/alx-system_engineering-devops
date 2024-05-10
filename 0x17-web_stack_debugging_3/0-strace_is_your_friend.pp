c93e0a04b99c7af2996cd11742381388b72ff181
Buchiken11 <buchiken025@gmail.com>
2024-05-10 13:30:32 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# To fix apache bad requset in wp-settings.php
+# Puppet script to fix "bad request" issue in wp-settings.php
 
 exec{'fix-wrdpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path	  => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/'
 }
